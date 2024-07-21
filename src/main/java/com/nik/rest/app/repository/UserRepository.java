c572726209d912f3787b9e3dfddaf80801951228
Nikhilesh Tippana <nikhilesh.nike@gmail.com>
2024-07-21 14:50:43 -0400

    remove java app

--- src/main/java/com/nik/rest/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nik.rest.app.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.nik.rest.app.entity.UserEntity;
-
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-	
-}
